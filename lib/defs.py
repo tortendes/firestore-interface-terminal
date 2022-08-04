8479d1307f761cd897f41bfa52e2debacd237766
Jarelle Emmanuel Payad Andulan <torten@proton.me>
2022-08-04 11:46:12 +0800

    Added requirements.txt, made everything a module.

---
+++ lib/defs.py
@@ -0,0 +1,38 @@
+ratings = [
+        ("Highly Recommended (HR)", 4), 
+        ("Recommended (R)", 3), 
+        ("Good (G)", 2), 
+        ("Decent (D)", 1), 
+        ("Meh (M)", 0)
+        ]
+
+chapterLength = [
+        ('Under 100 Chapters (UH)', 0), 
+        ('Over 100 Chapters (OH)', 1)
+        ]
+tags = [
+        "Zombies",
+        "Unique Cheat System",
+        "Trapped",
+        "Swords/Magic Academy",
+        "Sports",
+        "Side Character/Extra",
+        "School",
+        "Revenge",
+        "Returnee",
+        "Reincarnation",
+        "Regression",
+        "Non-Combat",
+        "Necromancer",
+        "Murim",
+        "Modern World Cheat",
+        "Medieval Fantasy",
+        "Medical",
+        "Isekai",
+        "Game",
+        "Entertainment Industry",
+        "Dungeons/Towers and Hunters/Rankers",
+        "Different Weapon",
+        "Cooking"
+        ]
+
---
+++ methods/add.py
@@ -0,0 +1,94 @@
+import inquirer
+from halo import Halo
+from lib.defs import ratings, chapterLength, tags
+
+manhwa = {}
+manhwa['data'] = {}
+manhwa['sources'] = []
+manhwa['notes'] = []
+
+def initialQuestions():
+    questions = [
+        inquirer.Text('title', message="Please enter Title"),
+        inquirer.Editor('description', message="Please add Description"),
+        inquirer.Editor('image', message="Please add the Image Link for the title, the bigger, the better."),
+        inquirer.Checkbox(
+            "tags",
+            message="Please add the coressponding tags that fit this title.",
+            choices=tags
+            ),
+        inquirer.List(
+            "chaplen",
+            message="How many chapter there is in this Title?",
+            choices=chapterLength
+            ),
+        inquirer.List(
+            "rating",
+            message="What's the rating for this title",
+            choices=ratings
+            ),
+        inquirer.Confirm('finished', message="Is this title Completed?")
+    ]
+    answers = inquirer.prompt(questions)
+
+    manhwa["title"] = answers["title"]
+    manhwa["description"] = answers["description"]
+    manhwa["imageLink"] = answers["image"]
+    manhwa["data"]["chapCount"] = answers["chaplen"]
+    manhwa["data"]["rating"] = answers["rating"]
+    manhwa["data"]["completed"] = answers["finished"]
+    manhwa["data"]["tags"] = answers["tags"]
+    print(answers)
+
+def sourcesQuestion():
+    questions = [
+        inquirer.Text('sitename', message="Enter the site name you're adding"),
+        inquirer.Editor('sitelink', message="Please enter the link for this link."),
+        inquirer.List('from', message="What is this site?", choices=[("Agressor", 0), ("Scan Group", 1), ("Official", 3)])
+    ]
+    sourceanswer = inquirer.prompt(questions)
+    manhwa['sources'].append(sourceanswer)
+
+    question2 = [
+            inquirer.Confirm('again', message="Do you want to add another source?")
+        ]
+    ans = inquirer.prompt(question2)
+    if ans["again"] == True:
+        sourcesQuestion()
+    else:
+        return
+
+def notesQuestion():
+    preliminary = [
+            inquirer.Confirm('notes', message="Do you want to add notes?")
+        ]
+    addnote = inquirer.prompt(preliminary)
+    if addnote['notes'] == False:
+        return
+
+    questions = [
+            inquirer.Editor('note', message="What is the note would you want to add?")
+        ]
+    noteans = inquirer.prompt(questions)
+    manhwa['notes'].append(noteans['note'])
+
+    question2 = [
+            inquirer.Confirm('again', message="Do you want to add another note?")
+        ]
+    ans = inquirer.prompt(question2)
+    if ans["again"] == True:
+        notesQuestion()
+    else:
+        return
+
+def add(db):
+    initialQuestions()
+    sourcesQuestion()
+    notesQuestion()
+
+    spinner = Halo(text="Inserting Data To Database", spinner="dots")
+    spinner.start()
+
+    db.collection('titles').add(manhwa)
+
+    spinner.succeed('Added a title to the database successfully.')
